a2e603b42d0a4033d3306b6b0887a63f6b023dc8
Rohan Das <mailtorohan.97@gmail.com>
2024-09-16 18:21:55 +0530

    Add files via upload

---
+++ Trip_Cancellation_Percentage.sql
@@ -0,0 +1,39 @@
+Create table  Trips (id int, client_id int, driver_id int, city_id int, status varchar(50), request_at varchar(50));
+Create table Users (users_id int, banned varchar(50), role varchar(50));
+
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('1', '1', '10', '1', 'completed', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('2', '2', '11', '1', 'cancelled_by_driver', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('3', '3', '12', '6', 'completed', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('4', '4', '13', '6', 'cancelled_by_client', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('5', '1', '10', '1', 'completed', '2013-10-02');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('6', '2', '11', '6', 'completed', '2013-10-02');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('7', '3', '12', '6', 'completed', '2013-10-02');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('8', '2', '12', '12', 'completed', '2013-10-03');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('9', '3', '10', '12', 'completed', '2013-10-03');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('10', '4', '13', '12', 'cancelled_by_driver', '2013-10-03');
+
+insert into Users (users_id, banned, role) values ('1', 'No', 'client');
+insert into Users (users_id, banned, role) values ('2', 'Yes', 'client');
+insert into Users (users_id, banned, role) values ('3', 'No', 'client');
+insert into Users (users_id, banned, role) values ('4', 'No', 'client');
+insert into Users (users_id, banned, role) values ('10', 'No', 'driver');
+insert into Users (users_id, banned, role) values ('11', 'No', 'driver');
+insert into Users (users_id, banned, role) values ('12', 'No', 'driver');
+insert into Users (users_id, banned, role) values ('13', 'No', 'driver');
+
+select * from Trips;
+select * from Users;
+-- cancelation rate of the unbanned users day b/w 2013-10-01 & 2013-10-03
+-- cancellation rate -> # of cancelled requests(by client/driver) requests with unbanned by the total
+-- no of equests by the unbanned user on that date
+
+select request_at, 
+count(case when status in ('cancelled_by_client','cancelled_by_driver') then 1 else null end) as cancle,
+count(1) total_trips, 
+count(case when status in ('cancelled_by_client','cancelled_by_driver') then 1 else null end)/count(1) total_trips
+from
+trips t
+inner join  users c on t.client_id = c.users_id 
+inner join  users d on t.client_id = d.users_id 
+where c.banned = 'No' and d.banned = 'No'
+group by request_at;
