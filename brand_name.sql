4c09aba22ad2e0de692041e678f15455883802c1
Rohan Das <mailtorohan.97@gmail.com>
2024-09-26 23:11:43 +0530

    Add files via upload

---
+++ brand_name.sql
@@ -0,0 +1,30 @@
+create table brands 
+(
+category varchar(20),
+brand_name varchar(20)
+);
+insert into brands values
+('chocolates','5-star')
+,(null,'dairy milk')
+,(null,'perk')
+,(null,'eclair')
+,('Biscuits','britannia')
+,(null,'good day')
+,(null,'boost');
+
+select * from brands;
+
+-- populate category values to the last not null values
+
+with cte1 as(
+select *,
+row_number() over(order by (select null)) rn
+from brands
+),cte2 as(
+select * 
+, lead(rn,1,9999) over(order by rn) next_rn
+from cte1 where category is not null
+)
+select cte2.category, cte1.brand_name from
+cte1 inner join cte2 on cte1.rn >= cte2.rn and cte1.rn <= cte2.next_rn -1
+
