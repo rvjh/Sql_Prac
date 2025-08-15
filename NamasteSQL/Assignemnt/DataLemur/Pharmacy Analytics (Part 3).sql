5e26bcc54d219848006321a3d3abdb103d2e64f9
Rohan Das <mailtorohan.97@gmail.com>
2025-08-15 08:51:39 +0530

    Add files via upload

---
+++ NamasteSQL/Assignemnt/DataLemur/Pharmacy Analytics (Part 3).sql
@@ -0,0 +1,6 @@
+SELECT 
+  manufacturer, 
+  CONCAT( '$', ROUND(SUM(total_sales) / 1000000), ' million') AS sales_mil 
+FROM pharmacy_sales 
+GROUP BY manufacturer 
+ORDER BY SUM(total_sales) DESC, manufacturer;
