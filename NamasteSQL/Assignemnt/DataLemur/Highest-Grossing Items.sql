dab6aecdd0e38cae68a74a5308588169ffe080f0
Rohan Das <mailtorohan.97@gmail.com>
2025-08-16 18:50:08 +0530

    Add files via upload

---
+++ NamasteSQL/Assignemnt/DataLemur/Highest-Grossing Items.sql
@@ -0,0 +1,20 @@
+WITH ranked_spending_cte AS (
+  SELECT 
+    category, 
+    product, 
+    SUM(spend) AS total_spend,
+    RANK() OVER (
+      PARTITION BY category 
+      ORDER BY SUM(spend) DESC) AS ranking 
+  FROM product_spend
+  WHERE EXTRACT(YEAR FROM transaction_date) = 2022
+  GROUP BY category, product
+)
+
+SELECT 
+  category, 
+  product, 
+  total_spend 
+FROM ranked_spending_cte 
+WHERE ranking<=2
+ORDER BY category asc, total_spend desc;
