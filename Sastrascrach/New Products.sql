20ca4027fff99da78dfdbdfd4f299365bedde639
Rohan Das <mailtorohan.97@gmail.com>
2025-09-04 09:46:03 +0530

    Update New Products.sql

--- Sastrascrach/New Products.sql
+++ Sastrascrach/New Products.sql
@@ -6,4 +6,14 @@ from car_launches
 group by company_name)
 select company_name,
 (p_2020_cnt - p_2019_cnt) net_products
+
 from cte
+
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+dc_bikeshare_q1_2012.head()
+
+dc_bikeshare_q1_2012.groupby("bike_number")["end_time"].max().reset_index(name="last_used")
